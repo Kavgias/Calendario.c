2d0ed930a75053d1cfebcb12ea07c0913cdecba7
Leandro kavgias <128564089+Kavgias@users.noreply.github.com>
2024-05-02 17:11:35 -0300

    calendario.c

--- calendario.c
+++ calendario.c
@@ -31,9 +31,6 @@ int calcularDiaSemana(int mes) {
   for (int i = 0; i < mes; i++) {
     totalDias += mesDias[i];
   }
-  // // considera o ano bissexto, se aplicável
-  // if (mes > 1 && anoBi)
-  //   totalDias++;
   // adiciona o deslocamento do dia da semana inicial
   return (diaSemanaI + totalDias) % 7;
 }
